7620e8debae6f82a5ce2493bee6041e02e6f17cc
Chiffa <artemy040404@gmail.com>
2025-04-20 12:41:11 +0300

    server->fix: remove authorization

--- MallenomTest.Server/Program.cs
+++ MallenomTest.Server/Program.cs
@@ -12,9 +12,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         
-        // Add services to the container.
-        builder.Services.AddAuthorization();
-        
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
         builder.Services.AddEndpointsApiExplorer();
